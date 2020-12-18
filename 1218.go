604194c3eb93b5d837bf4592b8e198b98fce9e81
FengChan <1224574671@qq.com>
2020-12-18 16:49:07 +0800

    1218 389. 找不同

---
+++ 1218.go
@@ -0,0 +1,17 @@
+package leetcode
+
+//https://leetcode-cn.com/problems/find-the-difference/submissions/
+func findTheDifference(s string, t string) byte {
+	m := make([]int, 26)
+	for _, v := range []byte(s) {
+		m[v-'a']++
+	}
+	for _, v := range []byte(t) {
+		if m[v-'a'] == 0 {
+			return v
+		} else {
+			m[v-'a']--
+		}
+	}
+	return ' '
+}
